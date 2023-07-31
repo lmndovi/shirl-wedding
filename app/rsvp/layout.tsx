d9fee7cea83fca636178bed5a91b0f01e7cafef7
lmndovi <l.ndovi92@gmail.com>
2023-08-01 00:28:11 +0100

    Changed padding on rsvp page for mobile

--- app/rsvp/layout.tsx
+++ app/rsvp/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-1 h-full w-full bg-floral-mobile bg-cover bg-center md:text-center text-[#313638] pb-10">
+    <div className="flex flex-1 h-full w-full bg-floral-mobile bg-cover bg-center md:text-center text-[#313638] pb-10 px-8 md:px-0">
       <div className="flex">
         <h1 className="mx-auto font text-lg font-semibold font-sans text-white">
           {children}
